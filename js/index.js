196706c0ca087ad7d8a5606dddd219d5c7b3d4cc
rana0105 <ra.ranacse@gmail.com>
2018-03-14 15:57:05 +0600

    Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'I want to learn vuejs with expertness'
+    title: 'I want to learn vuejs with expertness',
+    test: 'I want to learn vuejs with expertness',
+    link: 'https://translate.google.com.bd/?hl=en',
+    linkTest: '<a target="blank" href="https://google.com">Google</a>'
   },
   methods: {
   	changeTitle: function (event) {
   		this.title = event.target.value;
+  	}, 
+  	callData: function () {
+  		this.test = 'Hello!';
+  		return this.test;
   	}
   }
 });
