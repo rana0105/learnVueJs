b3c12e9336796c4c77582c127d3fc693d4e77ccd
rana0105 <ra.ranacse@gmail.com>
2018-03-15 16:33:55 +0600

    Update

--- js/index.js
+++ js/index.js
@@ -4,7 +4,12 @@ new Vue({
     title: 'I want to learn vuejs with expertness',
     test: 'I want to learn vuejs with expertness',
     link: 'https://translate.google.com.bd/?hl=en',
-    linkTest: '<a target="blank" href="https://google.com">Google</a>'
+    linkTest: '<a target="blank" href="https://google.com">Google</a>',
+    counter: 0,
+    result: '',
+    x: 0,
+    y: 0,
+    name: 'Rana'
   },
   methods: {
   	changeTitle: function (event) {
@@ -13,6 +18,27 @@ new Vue({
   	callData: function () {
   		this.test = 'Hello!';
   		return this.test;
-  	}
+  	},
+    increase: function (step, event) {
+      this.counter +=step;
+    },
+    updateCoordinates: function (event) {
+      this.x = event.clientX;
+      this.y = event.clientY;
+    },
+    alertMe: function () {
+      alert('Please input value');
+    },
+    increased: function () {
+      this.counter++;
+      this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    },
+    decreasei: function () {
+      this.counter--;
+      this.result = this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    },
+    results(){
+      return this.counter > 5 ? 'Greater than 5' : 'Smaller than 5';
+    }
   }
 });
